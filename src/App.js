f484a0a74c277869a0f3e97385b51307c9fdecce
Denis <ddujota@internationalsafety.com>
2021-07-31 17:03:12 -0400

    hooks started

--- src/App.js
+++ src/App.js
@@ -8,38 +8,17 @@ import './App.css';
 
 // condtionially render some content based on the new state variable
 
-class App extends React.Component {
 
-  state = {showContent: false}
+function App() {
+  return (
+  <div class="App">
 
-  handleShowPage = () => {
-    const {showContent} = this.state;
-    this.setState({showContent: !showContent})
-  }
-
-  render(){
-    const {showContent} = this.state;
-    return (
-      <div class="App">
-
-        {showContent && <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro at, itaque eos facere blanditiis unde similique laboriosam ipsa, delectus tempore magni corporis doloremque quia omnis velit enim totam atque dolor ut nulla, sit voluptas perspiciatis? Maiores voluptas non enim iure perferendis explicabo, quam odit praesentium facilis minima blanditiis placeat rem!
-        </div>}
-        <button onClick={this.handleShowPage}>Show/Hide</button>
-      </div>
-      );
-  }
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro at, itaque eos facere blanditiis unde similique laboriosam ipsa, delectus tempore magni corporis doloremque quia omnis velit enim totam atque dolor ut nulla, sit voluptas perspiciatis? Maiores voluptas non enim iure perferendis explicabo, quam odit praesentium facilis minima blanditiis placeat rem!
+    </div>
+    <button>Show/Hide</button>
+  </div>
+  );
 }
-// function App() {
-//   return (
-//   <div class="App">
-
-//     <div class="content">
-//       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro at, itaque eos facere blanditiis unde similique laboriosam ipsa, delectus tempore magni corporis doloremque quia omnis velit enim totam atque dolor ut nulla, sit voluptas perspiciatis? Maiores voluptas non enim iure perferendis explicabo, quam odit praesentium facilis minima blanditiis placeat rem!
-//     </div>
-//     <button>Show/Hide</button>
-//   </div>
-//   );
-// }
 
 export default App;
