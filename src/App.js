7818df49b8f2c6cbc29003be8ce124a7ae802065
Denis <ddujota@internationalsafety.com>
2021-07-31 17:00:36 -0400

    class toggle finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// keep track of a variable showContent : <Boolean>
+// function that we can trigger which  will set true/false
+// call that function when the button is clicked
+
+// condtionially render some content based on the new state variable
+
+class App extends React.Component {
+
+  state = {showContent: false}
+
+  handleShowPage = () => {
+    const {showContent} = this.state;
+    this.setState({showContent: !showContent})
+  }
+
+  render(){
+    const {showContent} = this.state;
+    return (
+      <div class="App">
+
+        {showContent && <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro at, itaque eos facere blanditiis unde similique laboriosam ipsa, delectus tempore magni corporis doloremque quia omnis velit enim totam atque dolor ut nulla, sit voluptas perspiciatis? Maiores voluptas non enim iure perferendis explicabo, quam odit praesentium facilis minima blanditiis placeat rem!
+        </div>}
+        <button onClick={this.handleShowPage}>Show/Hide</button>
+      </div>
+      );
+  }
 }
+// function App() {
+//   return (
+//   <div class="App">
+
+//     <div class="content">
+//       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro at, itaque eos facere blanditiis unde similique laboriosam ipsa, delectus tempore magni corporis doloremque quia omnis velit enim totam atque dolor ut nulla, sit voluptas perspiciatis? Maiores voluptas non enim iure perferendis explicabo, quam odit praesentium facilis minima blanditiis placeat rem!
+//     </div>
+//     <button>Show/Hide</button>
+//   </div>
+//   );
+// }
 
 export default App;
